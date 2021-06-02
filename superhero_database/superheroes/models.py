f1dfeaa8d2b7fc05b6aca09c6b6234ba1f56ac68
unknown <schmidt1519@gmail.com>
2021-06-02 16:34:45 -0500

    Added templates folder and urls.py in superheroes app folder to register url endpoints to the project list. Updated views.py with the index method to render the superheroes index page. Redirected the default address to the superheroes index page. Created index.html to render a template. Used the admin interface to add 4 superheroes. Refreshed DB and viewed superheroes in MySQL. Updated views.py index method to create dictionary of all superheroes and render them to index.html. Updated index.hmtl with logic to display the list of superheroes on the webpage.

--- superhero_database/superheroes/models.py
+++ superhero_database/superheroes/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class Superhero(models.Model):
     name = models.CharField(max_length=50)
     alter_ego = models.CharField(max_length=50)
-    primary_ability = models.CharField(max_length=50)
-    secondary_ability = models.CharField(max_length=50)
+    primary_ability = models.CharField(max_length=100)
+    secondary_ability = models.CharField(max_length=100)
     catchphrase = models.CharField(max_length=50)
 
     def __str__(self):
--- superhero_database/superheroes/views.py
+++ superhero_database/superheroes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Superhero
 # Create your views here.
+
+
+def index(request):
+    all_superheroes = Superhero.objects.all()
+    context = {
+        "all_superheroes": all_superheroes
+    }
+    return render(request, 'superheroes/index.html', context)
+
+
+def detail(request, superhero_id):
