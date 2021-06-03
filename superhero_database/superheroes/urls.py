99aceea74752ded8ef3121e8b3cb73d6c4bc9caf
unknown <schmidt1519@gmail.com>
2021-06-03 16:55:03 -0500

    Added bootstrap links/scripts to each html page. Created a styles.css sheet and linked to index.html. Added static image of favorite superhero to index page. Working on styling each html page next.

--- superhero_database/superheroes/urls.py
+++ superhero_database/superheroes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 app_name = 'superheroes'
 urlpatterns = [
@@ -9,4 +10,4 @@ urlpatterns = [
     path('new/', views.create, name='create'),
     path('edit/<int:superhero_id>/', views.edit, name='edit'),
     path('delete/<int:superhero_id>/', views.delete, name='delete')
-]
+] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
