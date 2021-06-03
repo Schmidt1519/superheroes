c36dbf1ec336c9fdcaf3afd3b55d45ed58159011
unknown <schmidt1519@gmail.com>
2021-06-02 20:08:37 -0500

    Added placeholder method for detail on views.py, a href on index.html for details, and a details path on superheroes url file. Added create.html with a form for adding new superheroes. Updated superheroes urls.py with a new path for newly created superheros. Added create method to views.py assigning the new entries a variable and creating a new superhero object and adding it to the index webpage list. Working on building out the detail method next.

--- superhero_database/superheroes/views.py
+++ superhero_database/superheroes/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Superhero
+from django.urls import reverse
 # Create your views here.
 
 
 def index(request):
     all_superheroes = Superhero.objects.all()
-    context = {
-        "all_superheroes": all_superheroes
-    }
+    context = {"all_superheroes": all_superheroes}
     return render(request, 'superheroes/index.html', context)
 
 
 def detail(request, superhero_id):
+    pass
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_ability = request.POST.get('primary_ability')
+        secondary_ability = request.POST.get('secondary_ability')
+        catchphrase = request.POST.get('catchphrase')
+        new_superhero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary_ability, secondary_ability=secondary_ability, catchphrase=catchphrase)
+        new_superhero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
