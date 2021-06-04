f2986ed3fa15d7b453c581563119f78177a34191
unknown <schmidt1519@gmail.com>
2021-06-03 22:46:39 -0500

    updated each html page with a dark background, containers on the content, turned links into buttons, and added a superhero image to the index page.

--- superhero_database/superheroes/urls.py
+++ superhero_database/superheroes/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-# from django.conf import settings
-# from django.conf.urls.static import static
 
 app_name = 'superheroes'
 urlpatterns = [
@@ -10,4 +8,4 @@ urlpatterns = [
     path('new/', views.create, name='create'),
     path('edit/<int:superhero_id>/', views.edit, name='edit'),
     path('delete/<int:superhero_id>/', views.delete, name='delete')
-] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
